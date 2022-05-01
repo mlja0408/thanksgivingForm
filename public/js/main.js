25ffcd158b9561e6c17151e248c7e8d9d9a37294
mlja0408 <80475650+mlja0408@users.noreply.github.com>
2022-05-01 13:45:55 -0700

    Added functionality to export table data as csv.

--- public/js/main.js
+++ public/js/main.js
@@ -39,3 +39,50 @@ function writeData() {
     `
     }
 }
+function download_csv(csv, filename) {
+    let csvFile;
+    let downloadLink;
+
+    // CSV FILE
+    csvFile = new Blob([csv], { type: "text/csv" });
+
+    // Download link
+    downloadLink = document.createElement("a");
+
+    // File name
+    downloadLink.download = filename;
+
+    // We have to create a link to the file
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+
+    // Make sure that the link is not displayed
+    downloadLink.style.display = "none";
+
+    // Add the link to your DOM
+    document.body.appendChild(downloadLink);
+
+    // Lanzamos
+    downloadLink.click();
+}
+
+function export_table_to_csv(html, filename) {
+    let csv = [];
+    let rows = document.querySelectorAll("table tr");
+
+    for (let i = 0; i < rows.length; i++) {
+        let row = [], cols = rows[i].querySelectorAll("td, th");
+
+        for (let j = 0; j < cols.length; j++)
+            row.push(cols[j].innerText);
+
+        csv.push(row.join(","));
+    }
+
+    // Download CSV
+    download_csv(csv.join("\n"), filename);
+}
+
+function  downloadCSV() {
+    let html = document.querySelector("table").outerHTML;
+    export_table_to_csv(html, "boxList.csv");
+};
